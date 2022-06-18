961bca28805365d4799194512a1278f0ce53d741
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-18 20:32:20 +0300

    refactor: refactor nodemon, ts, package configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 
-// export const PORT =  process.env.PORT || 3000
-export const { PORT = 3000 } =  process.env
+ export const PORT =  process.env.PORT || 3000
+// export const { PORT = 3000 } =  process.env
